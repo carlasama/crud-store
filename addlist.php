a42165c9b65979dca8e582dbb82552229b886b9b
carlasama <84520565+carlasama@users.noreply.github.com>
2021-07-06 21:11:35 -0400

    Add files via upload

---
+++ addlist.php
@@ -0,0 +1,181 @@
+<?php
+session_start();
+require_once "Database/Database.php";
+if ($_SESSION['username'] == null) {
+    echo "<script>alert('Please login.');</script>";
+    header("Refresh:0 , url=index.html");
+}
+$username = $_SESSION['username'];
+$sql_fetch_todos = "SELECT * FROM product ORDER BY id ASC";
+$query = mysqli_query($conn, $sql_fetch_todos);
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Desenvolvimento Web 1</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="dp.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-color: rgb(242, 215, 213);
+        }
+
+        .header p {
+            margin-left: 20px;
+            text-align: left;
+        }
+        .button-logout:hover {
+            background-color: #D9ddd4;
+            color: red;
+        }
+        .container {
+            margin: 90px auto;
+            margin-bottom: 50px;
+            border-radius: 30px;
+            text-align: center;
+            background-color: white;
+            width: 40%;
+            padding-bottom: 10px;
+        }
+
+        table th,
+        tr,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 10px 0px 10px 0px;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        th {
+            color: white;
+            background-color: #F1948A;
+        }
+
+        tr {
+            background-color: white;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .timeregis {
+            text-align: center;
+        }
+
+        .form-group {
+            margin-left: 600px;
+        }
+
+        [type=text], [type=number] {
+            font-family: "Lucida Console", "Courier New", monospace;
+            border-radius: 15px;
+            border: transparent;
+            padding: 7px 200px 7px 5px;
+        }
+
+        .return {
+            border-radius: 15px;
+            background-color: #ffcc33;
+            color: black;
+            text-decoration: none;
+            padding: 4px 40px 4px 40px;
+            margin: 0px 0px 50px 100px;
+            font-size: 20px;
+            transition: 0.5s;
+
+        }
+
+        .return:hover {
+            background-color: #fdb515;
+            color: white;
+        }
+
+        .modify {
+            border-radius: 15px;
+            border: transparent;
+            color: white;
+            padding: 4px 40px 4px 40px;
+            margin: 0px 50px 50px 100px;
+            font-size: 20px;
+            border-collapse: collapse;
+            background-color: #00A600;
+            font-family: "Mitr", sans-serif;
+            transition: 0.5s;
+
+        }
+
+        .modify:hover {
+            color: black;
+            background-color: #BBFFBB;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Lista de produtos:</h1>
+        <h2>Olá : [<?php echo $username ?>]</h2>
+    </div>
+    <div class="table-product">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">Ordem</th>
+                <th scope="col">ID:Produto</th>
+                <th scope="col">Lista</th>
+                <th scope="col">Quantidade</th>
+                <th scope="col">Data Registro:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $idpro = 1;
+                while ($row = mysqli_fetch_array($query)) { ?>
+                    <tr>
+                        <td scope="row"><?php echo $idpro ?></td>
+                        <td><?php echo $row['id'] ?></td>
+                        <td><?php echo $row['proname'] ?></td>
+                        <td><?php echo $row['amount'] ?></td>
+                        <td class="timeregis"><?php echo $row['time'] ?></td>
+                    </tr>
+                <?php
+                    $idpro++;
+                } ?>
+            </tbody>
+        </table>
+        <br>
+        <div class="addproduct">
+            <form method="POST" action="main/addlist.php">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Listar: </label>
+                    <br>
+                    <input type="text" class="form-control" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Quantidade: </label>
+                    <br>
+                    <input type="number" class="form-control" name="amount" required> </div> <br>
+                <div class="form-button">
+                    <button type="submit" class="modify" style="float:right">Adicionar</button>
+                    <a name="" id="" class="return" href="list.php" role="button" style="float:left">Voltar</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php
+    mysqli_close($conn);
+    ?>
+</body>
+
+</html>
